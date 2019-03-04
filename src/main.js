68955e7f5c33c5301015fb562e909993d5dade6d
Phillip Hilliard <philliard3>
2019-03-04 08:46:03 -0500

    Moved vuetify config to plugins folder.

--- src/main.js
+++ src/main.js
@@ -5,17 +5,6 @@ import router from "./router";
 import store from "./store";
 import "./plugins/vuetify";
 import "vuetify/dist/vuetify.min.css";
-import Vuetify from "vuetify/lib";
-
-Vue.use(Vuetify, {
-	primary: "#4caf50",
-	secondary: "#2196f3",
-	accent: "#3f51b5",
-	error: "#f44336",
-	warning: "#ffc107",
-	info: "#607d8b",
-	success: "#8bc34a"
-});
 
 Vue.config.productionTip = false;
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,12 @@ import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
-	iconfont: "md"
+	iconfont: "md",
+	primary: "#4caf50",
+	secondary: "#2196f3",
+	accent: "#3f51b5",
+	error: "#f44336",
+	warning: "#ffc107",
+	info: "#607d8b",
+	success: "#8bc34a"
 });
