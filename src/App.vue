3969ce5e4c82a13f0e2c002c5fa469c96e000494
Phillip Hilliard <philliard3>
2019-04-25 16:44:05 -0400

    Updated navigation bar.

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,25 @@
 		<v-app>
 			<v-toolbar>
 				<v-toolbar-items>
-					<v-btn flat to="/">Home</v-btn>
-					<v-btn flat to="/jsx">JSX</v-btn>
-					<v-btn flat to="/initialize">New Calendar</v-btn>
-					<v-btn flat to="/createfiscalyear">New Fiscal Year</v-btn>
-					<v-btn flat to="/createscheduleblock">New Schedule Block</v-btn>
-					<v-btn flat to="/calendar">Calendar View</v-btn>
+					<v-btn flat to="/"> <v-icon dark>home</v-icon>Start </v-btn>
+					<v-btn flat to="/createfiscalyear">
+						<v-icon dark>library_add</v-icon>New Fiscal Year
+					</v-btn>
+					<v-btn flat to="/fiscalyearconstraints">
+						<v-icon dark>ballot</v-icon>FY Constraints
+					</v-btn>
+					<v-btn flat to="/createscheduleblock">
+						<v-icon dark>note_add</v-icon>New Schedule Block
+					</v-btn>
+					<v-btn flat to="/schedlueblockconstraints">
+						<v-icon dark>ballot</v-icon>Schedule Block Constraints
+					</v-btn>
+					<v-btn flat to="/manageemployees">
+						<v-icon dark>people</v-icon>Manage Employees
+					</v-btn>
+					<v-btn flat to="/calendar">
+						<v-icon dark>calendar_today</v-icon>Calendar View
+					</v-btn>
 				</v-toolbar-items>
 			</v-toolbar>
 			<router-view />
