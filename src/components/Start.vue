d2c678c3cfea8a1316318f0788972f0d91506ef1
Phillip Hilliard <philliard3>
2019-04-25 16:43:04 -0400

    Added new landing page and named it accordingly.

---
+++ src/components/Start.vue
@@ -0,0 +1,32 @@
+<template>
+	<v-container grid-list-md text-xs-center>
+		<v-layout row wrap>
+			<v-flex xs2></v-flex>
+			<v-flex xs3>
+				<v-btn color="success" @click="loadFile">Load Calendar from File</v-btn>
+			</v-flex>
+			<v-flex xs2></v-flex>
+			<v-flex xs3>
+				<v-btn color="info" to="/createfiscalyear">New Calendar</v-btn>
+			</v-flex>
+			<v-flex xs2></v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+export default {
+	name: "Start",
+	data() {
+		return {};
+	},
+	methods: {
+		// This function loads a file from local storage.
+		// In the electron build, it should call to the main process.
+		// In the webapp build, it should use the browser's file upload API.
+		async loadFile() {
+			// console.log(process.env);
+		}
+	}
+};
+</script>
---
+++ src/views/Start.vue
@@ -0,0 +1,13 @@
+<template>
+	<Start />
+</template>
+
+<script>
+import Start from "../components/Start";
+
+export default {
+	components: {
+		Start
+	}
+};
+</script>
