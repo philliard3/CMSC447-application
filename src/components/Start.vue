b5cf3590ce07efd20cf08045cddfc148ac6f35b6
Phillip Hilliard <philliard3>
2019-05-03 13:33:29 -0400

    Adjusted spacing to be more appealing visually and more idomatic with Vuetify.

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,21 +1,27 @@
 <template>
 	<v-container grid-list-md text-xs-center align-center>
-		<div
-			id="loading-info"
-			class="title font-weight-medium"
-			v-if="currentBlock === null || currentBlock.sbID === null"
-		>
-			It looks like you don't have anything loaded yet. Select an option below
-			to get started.
-			<p />
-			<v-btn color="success" @click="loadFile">Load Calendar from File</v-btn>
-			<p />
-			<v-btn color="info" to="/fiscalyear/create">New Calendar</v-btn>
-		</div>
-		<div id="loading-info" class="title font-weight-medium" v-else>
-			You've already started on a calendar.
-			<v-btn color="info" to="/fiscalyear/create">New Fiscal Year</v-btn>
-		</div>
+		<v-card>
+			<v-card-text>
+				<v-container
+					id="loading-info"
+					class="title font-weight-medium"
+					v-if="currentBlock === null || currentBlock.sbID === null"
+				>
+					It looks like you don't have anything loaded yet. Select an option
+					below to get started.
+					<p />
+					<v-btn color="success" @click="loadFile"
+						>Load Calendar from File</v-btn
+					>
+					<p />
+					<v-btn color="info" to="/fiscalyear/create">New Calendar</v-btn>
+				</v-container>
+				<v-container id="loading-info" class="title font-weight-medium" v-else>
+					<v-container>You've already started on a calendar.</v-container>
+					<v-btn color="info" to="/fiscalyear/create">New Fiscal Year</v-btn>
+				</v-container>
+			</v-card-text>
+		</v-card>
 	</v-container>
 </template>
 
@@ -42,7 +48,5 @@ export default {
 	text-align: center;
 	max-width: 450px;
 	margin: auto;
-	margin-top: 7%;
-	margin-bottom: 20px;
 }
 </style>
