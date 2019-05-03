2b101f1bdcc3c5ca290203676a2077a027d5a18b
Phillip Hilliard <philliard3>
2019-05-03 14:10:17 -0400

    Added child components for EditEmployee view

---
+++ src/components/ShiftSelection.vue
@@ -0,0 +1,58 @@
+<template>
+	<v-container>
+		<v-card>
+			<v-card-text>
+				<div class="display-1 font-weight-light">Shift Preferences</div>
+				<v-container
+					fluid
+					v-for="(shift, shiftIndex) in $store.getters.currentScheduleBlock
+						.shifts"
+					:key="shift.name + shift.location + shift.time"
+					class="headline font-weight-medium"
+				>
+					{{ shift.name }} (Starts at {{ shift.startTime }} at location "{{
+						shift.location
+					}}")
+					<v-layout row wrap>
+						<v-flex
+							v-for="(dayList, dayListIndex) in shift.startDays.reduce(
+								(dayList, day, i) => {
+									if (i % 2) {
+										dayList[dayList.length - 1].push(day);
+									} else {
+										dayList.push([day]);
+									}
+									return dayList.slice();
+								},
+								[]
+							)"
+							:key="dayListIndex"
+							xs12
+							sm4
+							md4
+						>
+							<v-checkbox
+								v-for="day in dayList"
+								v-model="shifts[shiftIndex].selected"
+								:key="day"
+								:label="day"
+								:value="day"
+								hide-details
+							></v-checkbox>
+						</v-flex>
+					</v-layout>
+				</v-container>
+			</v-card-text>
+		</v-card>
+	</v-container>
+</template>
+
+<script>
+export default {
+	name: "ShiftSelection",
+	props: { shifts: Array },
+	data() {
+		return {};
+	}
+};
+</script>
---
+++ src/components/WorkPreferenceTable.vue
@@ -0,0 +1,36 @@
+<template>
+	<v-container>
+		<v-card>
+			<v-card-text>
+				<div class="display-1 font-weight-light">Work Day Preferences</div>
+				<v-container>
+					<v-layout>
+						<v-text-field
+							v-model="dayName"
+							label="Day name"
+							:rules="dayNameRules"
+							required
+						></v-text-field>
+					</v-layout>
+				</v-container>
+			</v-card-text>
+		</v-card>
+	</v-container>
+</template>
+
+<script>
+export default {
+	name: "WorkPreferenceTable",
+	props: { dayName: String, preferredDays: Array },
+	data() {
+		return {
+			dayNameRules: [
+				v => !!v || "Name is required to tell days apart.",
+				v =>
+					!this.preferredDays.some(day => day.name === v) ||
+					"Name cannot be already taken"
+			]
+		};
+	}
+};
+</script>
