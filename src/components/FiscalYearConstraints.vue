20ae2ad76c1abf80d244741455f64e45a16e6857
Phillip Hilliard <philliard3>
2019-05-16 01:00:22 -0400

    Moved Shift managment to its own view

--- src/components/FiscalYearConstraints.vue
+++ src/components/FiscalYearConstraints.vue
@@ -19,113 +19,23 @@
 				</v-card-text>
 			</v-card>
 		</v-container>
-		<v-container>
-			<v-card>
-				<v-card-text>
-					<div class="display-1 font-weight-light" id="header-row-1">
-						Shifts
-					</div>
-					<v-layout>
-						<v-flex xs11 sm5>
-							<v-text-field
-								name="Shift Nickname"
-								label="Shift Nickname"
-								v-model="newShiftData.name"
-							></v-text-field>
-						</v-flex>
-						<v-flex xs11 sm5>
-							<v-menu
-								ref="menu1"
-								v-model="menu1"
-								:close-on-content-click="false"
-								:nudge-right="40"
-								:return-value.sync="newShiftData.startTime"
-								lazy
-								transition="scale-transition"
-								offset-y
-								full-width
-								max-width="290px"
-								min-width="290px"
-							>
-								<template v-slot:activator="{ on }">
-									<v-text-field
-										v-model="newShiftData.startTime"
-										label="Shift Start Time"
-										prepend-icon="access_time"
-										readonly
-										v-on="on"
-									></v-text-field>
-								</template>
-								<v-time-picker
-									v-if="menu1"
-									v-model="newShiftData.startTime"
-									full-width
-									@click:minute="$refs.menu1.save(newShiftData.startTime)"
-								></v-time-picker>
-							</v-menu>
-						</v-flex>
-						<v-flex xs11 sm5>
-							<v-menu
-								ref="menu2"
-								v-model="menu2"
-								:close-on-content-click="false"
-								:nudge-right="40"
-								:return-value.sync="newShiftData.endTime"
-								lazy
-								transition="scale-transition"
-								offset-y
-								full-width
-								max-width="290px"
-								min-width="290px"
-							>
-								<template v-slot:activator="{ on }">
-									<v-text-field
-										v-model="newShiftData.endTime"
-										label="Shift End Time"
-										prepend-icon="access_time"
-										readonly
-										v-on="on"
-									></v-text-field>
-								</template>
-								<v-time-picker
-									v-if="menu2"
-									v-model="newShiftData.endTime"
-									full-width
-									@click:minute="$refs.menu2.save(newShiftData.endTime)"
-								></v-time-picker>
-							</v-menu>
-						</v-flex>
-					</v-layout>
-					<div>
-						<v-btn color="success" @click="createShift">
-							<v-icon flat>calendar_view_day</v-icon>&nbsp;New Shift
-						</v-btn>
-					</div>
-					<!--- shift table -->
-				</v-card-text>
-			</v-card>
-		</v-container>
+		<ManageShifts></ManageShifts>
 		<LocationList></LocationList>
 	</v-form>
 </template>
 
 <script>
 import LocationList from "./LocationList";
+import ManageShifts from "./ManageShifts";
 
 export default {
 	components: {
-		LocationList
+		LocationList,
+		ManageShifts
 	},
 	data() {
 		return {
-			valid: false,
-			menu1: false,
-			menu2: false,
-			newShiftData: {
-				name: "",
-				startTime: null,
-				endTime: null
-			}
+			valid: false
 		};
 	},
 	computed: {
@@ -178,19 +88,6 @@ export default {
 			this.$router.push("/fiscalyear/create");
 		}
 	},
-	methods: {
-		createShift() {
-			const newShiftData = {
-				shiftID: new Date().getTime(),
-				...this.newShiftData
-			};
-			for (let value of Object.values(newShiftData)) {
-				if (!value) {
-					return;
-				}
-			}
-			// console.log(newShiftData);
-		}
-	}
+	methods: {}
 };
 </script>
