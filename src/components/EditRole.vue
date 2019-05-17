ca3f3b99492f7618cad34270ea83346c4bc6a10b
Phillip Hilliard <philliard3>
2019-05-16 23:20:18 -0400

    Fixed colors so that they update in the store upon selection.

--- src/components/EditRole.vue
+++ src/components/EditRole.vue
@@ -1,14 +1,27 @@
 <template>
 	<v-form>
-		<v-container class="title font-weight-medium">
+		<v-container class="display-1 font-weight-light">
 			<v-card v-if="error">
 				<v-card-text>{{ error }}</v-card-text>
 			</v-card>
 			<v-card v-else>
 				<v-card-text>
-					<v-container>Employee Role: {{ roleData.name }}</v-container>
 					<v-container>
-						<swatches v-model="roleData.color" colors="basic" inline></swatches>
+						<v-layout>
+							<v-flex>Employee Role: {{ roleData.name }}</v-flex>
+						</v-layout>
+					</v-container>
+					<v-container>
+						<v-layout>
+							<v-flex>
+								<v-card-text class="title">Colors</v-card-text>
+							</v-flex>
+						</v-layout>
+						<v-layout>
+							<v-flex>
+								<swatches v-model="color" colors="basic" inline></swatches>
+							</v-flex>
+						</v-layout>
 					</v-container>
 					<v-container id="bad"></v-container>
 				</v-card-text>
@@ -39,10 +52,30 @@ export default {
 				return null;
 			}
 
-			if (!roleData.color) {
-				roleData.color = "#1FBC9C";
-			}
 			return roleData;
+		},
+		color: {
+			get() {
+				const roleID = Number(this.$route.params.roleID);
+				const roles = this.$store.getters.roles.filter(
+					role => role.roleID === roleID
+				);
+				let roleData = null;
+				if (roles.length) {
+					roleData = roles[0];
+				} else {
+					return null;
+				}
+
+				if (!roleData.color) {
+					return "#1FBC9C";
+				}
+				return roleData.color;
+			},
+			set(newColor) {
+				const newRoleData = { ...this.roleData, color: newColor };
+				this.$store.commit("updateRole", { roleData: newRoleData });
+			}
 		},
 
 		error() {
