27aad0608fbddd23d1638a3a7aa6e723fcaec81d
Phillip Hilliard <philliard3>
2019-05-19 19:35:03 -0400

    Updated the way the build compiles to make it easier to do file pathing.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
 	lintOnSave: false,
-	transpileDependencies: [/node_modules[/\\\\]vuetify[/\\\\]/]
+	transpileDependencies: [/node_modules[/\\\\]vuetify[/\\\\]/],
+	pluginOptions: {
+		electronBuilder: {
+			builderOptions: {
+				appId: "com.electron.CMSC447",
+				productName: "Nurse Roster Builder",
+				directories: {
+					buildResources: "build",
+					output: "dist_electron"
+				}
+			}
+		}
+	}
 };
